b666d6b7a6d7cd2b522ec2f73214f67450bd1c85
lavish-dungerpuriya <129578747+lavish-dungerpuriya@users.noreply.github.com>
2024-07-28 04:27:50 +0530

    Add files via upload

---
+++ Hotel-Room-Booking-Website/src/Components/Services/Services.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+
+// imports react-icons
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
+
+// imports components
+import Title from "../Title/Title";
+
+export default class Services extends Component {
+  state = {
+    services: [
+      {
+        icon: <FaCocktail />,
+        title: "free cocktails",
+        info:
+          "",
+      },
+      {
+        icon: <FaHiking />,
+        title: "endless hiking",
+        info:
+          "",
+      },
+      {
+        icon: <FaShuttleVan />,
+        title: "free shuttle",
+        info:
+          "",
+      },
+      {
+        icon: <FaBeer />,
+        title: "storages beer",
+        info:
+          "free wifi",
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <section className="services">
+        <Title title="services" />
+
+        <div className="services-center">
+          {this.state.services.map((item, index) => {
+            return (
+              <article key={index} className="services">
+                <span>{item.icon}</span>
+                <h6>{item.title}</h6>
+                <p>{item.info}</p>
+              </article>
+            );
+          })}
+        </div>
+      </section>
+    );
+  }
+}
